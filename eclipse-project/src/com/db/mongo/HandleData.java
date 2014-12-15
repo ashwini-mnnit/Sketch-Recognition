b4eb09243991c32638b0a69d1bd1d57690897c6d
shirsing <shirsing@tamu.edu>
2014-12-14 23:18:46 -0600

    Added Sketchml from Mongo

--- eclipse-project/src/com/db/mongo/HandleData.java
+++ eclipse-project/src/com/db/mongo/HandleData.java
@@ -1,10 +1,13 @@
 package com.db.mongo;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.json.JSONException;
 
 import com.classify.ClassifyData;
+import com.google.gson.Gson;
+import com.sketchMl.Sketch;
 import com.sketchshape.SrlShapeExtended;
 
 public class HandleData {
@@ -32,8 +35,16 @@ public class HandleData {
 			ProcessData dataObject = new ProcessData("localhost", 27017, "SketchRec");
 			dataObject.removeSouseData("Souse", "0b8c1460-6919-45bf-b6e3-b3776b69a40b");
 			dataObject.processSouseData("/home/shirsing/Downloads/17902.xml", "Souse");
-			
+			dataObject.insertMechanixData("/home/shirsing/Downloads/SketchData.xml", "Mechanix");
 			RetrieveData r = new RetrieveData("localhost", 27017, "SketchRec");
-			r.getSketchMlforSouseData("Souse", "0b8c1460-6919-45bf-b6e3-b3776b69a40b");
+			
+			 r.getSketchMlforSouseData("Souse", "0b8c1460-6919-45bf-b6e3-b3776b69a40b");
+			
+			 ArrayList<Sketch> s =r.getSketchMlforMechanixData("Mechanix", "b589d96f-ec00-4156-9bc8-b6f26bf28cc5");
+			
+			for (Sketch it :s) {
+				Gson gson = new Gson();
+				System.out.println(gson.toJson(it));
+			}
 		}
 	}
--- eclipse-project/src/com/parser/mechanix/MechanixParser.java
+++ eclipse-project/src/com/parser/mechanix/MechanixParser.java
@@ -1,4 +1,4 @@
-
+package com.parser.mechanix;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
