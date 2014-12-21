fd541b6a3f89320df644944677cdb2644140f92b
shirsing <shirsing@neo.tamu.edu>
2014-12-21 14:54:40 -0800

    Cleaning dummy code

--- eclipse-project/src/com/db/mongo/HandleData.java
+++ eclipse-project/src/com/db/mongo/HandleData.java
@@ -2,19 +2,13 @@ package com.db.mongo;
 
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.List;
-import java.util.UUID;
 
 import org.codehaus.jackson.JsonGenerationException;
 import org.codehaus.jackson.map.JsonMappingException;
 import org.json.JSONException;
 
-import com.classify.ClassifyData;
 import com.google.gson.Gson;
-import com.parser.mechanix.MechanixShape;
-import com.parser.mechanix.MechanixSketch;
 import com.sketchMl.Sketch;
-import com.sketchshape.SrlShapeExtended;
 
 public class HandleData {
 
@@ -28,16 +22,8 @@ public class HandleData {
 			ProcessData dataObject = new ProcessData("localhost", 27017, "SketchRec");
 			dataObject.removeAllSouseData("Souse");
 			dataObject.insertSrlData("C:\\Users\\shirsing\\Desktop\\Referral\\1839\\17902.xml", "Souse");
-			//dataObject.removeAllMechanixData("Mechanix");
-			//dataObject.insertMechanixData("/home/shirsing/Downloads/SketchData.xml", "Mechanix");
 			
 			RetrieveData r = new RetrieveData("localhost", 27017, "SketchRec");
-			//ArrayList<Sketch> sketchMlList =r.getSketchMlforSouseData("Souse", "0b8c1460-6919-45bf-b6e3-b3776b69a40b");
-			
-			//ArrayList<Sketch> sketchMlList = r.queryOnPrimitiveTypes("Souse", "0b8c1460-6919-45bf-b6e3-b3776b69a40b","caret");
-			//ArrayList<Sketch> sketchMlList = r.getSketchMlforMechanixData("Mechanix", "b589d96f-ec00-4156-9bc8-b6f26bf28cc5");
-			//ArrayList<Sketch> sketchMlList = r.getSimilarSketchMlforMechanixData("Mechanix", 3);
-			
 			ArrayList<Sketch> sketchMlList = r.getSketchMlDataFromMongo("Souse", "0b8c1460-6919-45bf-b6e3-b3776b69a40b");
 			for (Sketch it : sketchMlList) {
 				    Gson g = new Gson();
